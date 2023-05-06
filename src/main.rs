45d4f28272d9f478e4352e34c4ad599f35422555
thembones79 <michal.szumnarski@gmail.com>
2023-05-07 00:02:35 +0200

    imported bracket-lib

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use bracket_lib::prelude::*;
 
 struct State {}
-fn main() {
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+fn main() -> BError {
     println!("Hello, world!");
 }
