637811eeedc19197492ecee365c194d1e0de7a75
thembones79 <michal.szumnarski@gmail.com>
2023-05-11 23:37:45 +0200

    started adding player

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,26 @@ enum GameMode {
     Playing,
     End,
 }
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
 struct State {
     mode: GameMode,
 }
+
 impl State {
     fn new() -> Self {
         State {
             mode: GameMode::Menu,
         }
+    }
+
+    fn restart(&mut self) {
+        self.mode = GameMode::Playing;
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
@@ -24,6 +36,14 @@ impl State {
         ctx.print_centered(5, "Welcone to Flappy Dragon");
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -31,8 +51,17 @@ impl State {
         ctx.print_centered(5, "You are dead");
         ctx.print_centered(8, "(P) Play Game");
         ctx.print_centered(9, "(Q) Quit Game");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
+
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
@@ -42,6 +71,7 @@ impl GameState for State {
         }
     }
 }
+
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
