a389b7192312b9a39fb06b25a90608ba3425fb8f
thembones79 <michal.szumnarski@gmail.com>
2023-05-17 23:34:30 +0200

    started creating obstacles

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,23 @@ impl Player {
     }
 }
 
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+}
+
 struct State {
     player: Player,
     frame_time: f32,
